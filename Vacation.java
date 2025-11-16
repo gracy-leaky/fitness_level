1db8f67d41e8b1a4d68be40c23aa79319d7b1983
gracy-leaky <gracyleaky@gmail.com>
2025-11-16 16:51:48 +0300

    Implement Vacation cost calculation
    Fix method name and output formatting.

---
+++ Vacation.java
@@ -0,0 +1,12 @@
+public class VacationCalculator {
+public static double doublecalculateTotalCost(double accommodationCost,double mealcostc,double activists){
+return accommodationCost + mealCost + activityCost;
+}
+public static void main(String[] args) {
+double accommodation =750.50;
+double meals =350.25;
+double activities =350.00;
+double totalCost =calculateTotalCost(accommodation,meals,activities);
+System.out.println("Total Vacation Cost:+Total Cost")
+}
+}
